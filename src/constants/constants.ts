5056b8454bf02a41aa4eb8e2dad4428578282d01
Jayanth Peetla <ynv7jk@virginia.edu>
2025-01-06 19:28:37 -0500

    updated project content

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,35 +1,68 @@
 export const portfolioProjects = [
   {
-    year: "2022",
-    title: "Dark Saas Landing Page",
-    description: "Insert description...",
+    title: "Sadhana",
+    description:
+      "An iOS application designed for personal meditation tracking, facilitating users to connect with friends and engage in a support community.",
     results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
+      {
+        title:
+          "Coded with SwiftUI and integrated FirebaseAuth to handle user authentication. Utilized a NoSQL database to manage user data securely.",
+      },
+      {
+        title:
+          "Developed a dynamic social media component, enabling real-time post creation and interactive user communication. Tested extensively via white-box and black-box testing strategies.",
+      },
     ],
-    link: "https://youtu.be/4k7IdSLxh6w",
+    link: "https://github.com/jpeetla/Sadhana",
   },
   {
-    year: "2021",
-    title: "Light Saas Landing Page",
-    description: "Insert description...",
+    title: "Executive Web Scraper",
+    description:
+      "A robust web scraper API for identifying a company's top executives to aid in lead generation. This service scrapes relevant websites and uses AI to find target data.",
     results: [
-      { title: "Boosted sales by 20%" },
-      { title: "Expanded customer reach by 35%" },
-      { title: "Increased brand awareness by 15%" },
+      {
+        title:
+          "Replaced entire lead generation method generating 3,000+ leads weekly.",
+      },
+      {
+        title:
+          "Used SERP API for web search results, Puppeteer to extract relevant webpage content, and LLM to scrape any mentioned executives. Launched as Node.js API endpoint through Render, interfaced through two wrapper APIs on Vercel.",
+      },
+      {
+        title:
+          "Deployed user-interface through Next.js, utilizing Prisma database",
+      },
     ],
-    link: "https://youtu.be/7hi5zwO75yc",
+    link: "https://github.com/jpeetla/executive-web-scraper",
   },
   {
-    year: "2023",
-    title: "AI Startup Landing Page",
-    description: "Insert description...",
+    title: "JobMatch AI",
+    description:
+      "A web extension to recommend relevant job postings to a candidate based on their Linkedin profile.",
     results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
+      { title: "Used regularly by 100+ recruiters." },
+      {
+        title:
+          "Implemented LLM + RAG architecture to categorize candidates and find relevant roles.",
+      },
     ],
-    link: "https://youtu.be/Z7I5uSRHMHg",
+    link: "https://github.com/jpeetla/jobmatch-ai",
+  },
+  {
+    title: "Focus Friend",
+    description:
+      "An iOS application curating various proven techniques to combat student procrastination.",
+    results: [
+      {
+        title:
+          "Designed backend database structure through Google Firebase and wrote security rules to protect sensitive user information",
+      },
+      {
+        title:
+          "Integrated Core ML framework to curate personalized anti-procrastination techniques",
+      },
+      { title: "Downloaded & Used by 100+ users." },
+    ],
+    link: "https://github.com/jpeetla/focusfriend",
   },
 ];
